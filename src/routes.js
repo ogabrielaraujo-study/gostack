29136d3bd5402d07817fe783ebf1793676307b71
Gabriel Araujo <gabriiel66@gmail.com>
2019-08-24 17:01:46 -0300

    appointments

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
+import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
-
-import authMiddleware from './app/middlewares/auth';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,13 +20,23 @@ routes.get('/', async (req, res) => {
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-// token
+// require token
 routes.use(authMiddleware);
 
+// users
 routes.put('/users', UserController.update);
 
+// providers
 routes.get('/providers', ProviderController.index);
 
+// appointments
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+// schedules
+routes.get('/schedule', ScheduleController.index);
+
+// files
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
