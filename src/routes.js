208932bd982c90b818248b344967c79063a0feaf
Gabriel Araujo <gabriiel66@gmail.com>
2019-09-03 09:34:49 -0300

    notificações

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -32,9 +33,14 @@ routes.get('/providers', ProviderController.index);
 // appointments
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 // schedules
 routes.get('/schedule', ScheduleController.index);
+
+// notifications
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 // files
 routes.post('/files', upload.single('file'), FileController.store);
