2615d323951cc2de9957a4c55106fe66f43ffa89
Gabriel Araujo <gabriiel66@gmail.com>
2019-09-03 12:21:09 -0300

    listagem de horários disponíveis

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -29,6 +30,7 @@ routes.put('/users', UserController.update);
 
 // providers
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 // appointments
 routes.get('/appointments', AppointmentController.index);
