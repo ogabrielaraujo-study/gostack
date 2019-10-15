43d7aeb91eb55f92c0b5317dd8ba13ca64277b0f
Gabriel Araujo <gabriiel66@gmail.com>
2019-10-15 15:30:28 -0300

    validação yup

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,20 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Form, Input } from "@rocketseat/unform";
+import * as Yup from "yup";
 
 import logo from "~/assets/logo.svg";
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("Insira um e-amil válido")
+    .required("O e-mail é obrigatório"),
+  password: Yup.string()
+    .min(6, "No mínimo 6 caracteres")
+    .required("A senha é obrigatória"),
+});
+
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoStack" />
 
-      <form>
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
