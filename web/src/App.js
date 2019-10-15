da59a0534ba761ea81af21ffea1c3b3e590b2c59
Gabriel Araujo <gabriiel66@gmail.com>
2019-10-15 18:55:55 -0300

    auth token

--- web/src/App.js
+++ web/src/App.js
@@ -21,7 +21,7 @@ function App() {
           <Routes />
 
           <GlobalStyle />
-          <ToastContainer autoClose="3000" />
+          <ToastContainer autoClose={3000} />
         </Router>
       </PersistGate>
     </Provider>
